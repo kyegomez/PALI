4a879769c1e1bec5a538dd3da0b766c2716082be
Kye <kye@apacmediasolutions.com>
2023-07-30 15:28:17 -0400

    clean up

--- pali/model.py
+++ pali/model.py
@@ -1,68 +1,50 @@
 import torch
-from transformers import MT5Model, ViTModel, AutoTokenizer
+from optimus_prime import ViTransformerWrapper, XTransformer, Encoder
 
-class PaliTokenizer:
-    """
-    A tokenizer class for the Pali model.
-    
-    Attributes:
-        tokenizer (AutoTokenizer): The tokenizer for text.
-    """
-    def __init__(self, text_encoder_model='google/mt5-large'):
-        self.tokenizer = AutoTokenizer.from_pretrained(text_encoder_model)
+#pali composes of 
+#1, vision transformer 
+#2. Encoder decoder transformer(X transform)
 
-    def tokenize_texts(self, texts):
-        """
-        Tokenize given texts.
-        
-        Args:
-            texts (str): The text to be tokenized.
-        
-        Returns:
-            The tokenized texts.
-        """
-        return self.tokenizer(texts, return_tensors="pt", padding=True, truncation=True).input_ids
+vit = ViTransformerWrapper(
+    image_size=256,
+    patch_size=32,
+    attn_layers = Encoder(
+        dim=512,
+        depth=6,
+        heads=8
+    )
+)
 
-    def tokenize(self, sample):
-        """
-        Tokenizes given sample.
-        
-        Args:
-            sample: The sample to be tokenized.
-        
-        Returns:
-            A dictionary containing the tokenized text.
-        """
-        return {
-            "text_tokens": self.tokenize_texts(sample["text"]),
-        }
+pali = XTransformer(
+    dim=512,
+    enc_num_tokens=256,
+    enc_depth=6,
+    enc_heads=8,
+    enc_max_seq_len=1024,
+    dec_num_tokens=256,
+    dec_depth=6,
+    dec_heads=8,
+    dec_max_seq_len=1024
+)
 
+#training data
+img = torch.randn(1, 3, 256, 256)
+prompt = torch.randint(0, 256, (1, 1024)) # prompt
+prompt_mask = torch.ones(1, 1024).bool()
+output_text = torch.randint(0, 256, (1, 1024)) #target output text
 
-class Pali(torch.nn.Module):
-    """
-    The Pali model, a multimodal model combining an MT5 model for text and a ViT model for images.
+#train
+img_embeds = vit(
+    img, 
+    return_embeddings=True
+)
 
-    Attributes:
-        text_encoder_decoder (MT5Model): The text encoder-decoder model.
-        vision_encoder (ViTModel): The vision encoder model.
-    """
-    def __init__(self, text_encoder_model='google/mt5-large', vision_encoder_model='google/vit-large-patch16-224'):
-        super(Pali, self).__init__()
+loss = pali(
+    prompt,
+    output_text,
+    mask=prompt_mask,
+    src_prepend_embeds=img_embeds # will prepend image embeddings
+)
 
-        self.text_encoder_decoder = MT5Model.from_pretrained(text_encoder_model)
-        self.vision_encoder = ViTModel.from_pretrained(vision_encoder_model)
+loss.backward()
 
-    def forward(self, text_input, image_input):
-        """
-        Forward pass through the model.
-        
-        Args:
-            text_input (torch.Tensor): The text input to the model.
-            image_input (torch.Tensor): The image input to the model.
-        
-        Returns:
-            The output from the text encoder-decoder model.
-        """
-        vision_output = self.vision_encoder(image_input)
-        text_output = self.text_encoder_decoder(input_ids=text_input, encoder_outputs=vision_output.last_hidden_state)
-        return text_output
